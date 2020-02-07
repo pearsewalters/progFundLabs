33127a4f4db8b713c67f1d358d29b348869850d6
Sam Walters <samuel.p.walters@gmail.com>
2020-02-06 23:38:39 -0600

    Lab 2 updated to match critiques from Lab 1

--- Lab_2/lab2_Sam_Walters.py
+++ Lab_2/lab2_Sam_Walters.py
@@ -4,13 +4,19 @@
 ##  gives them some options, and reads an input of their selection. It then outputs a
 ##  message to the screen depending on the user's input.
 
-# 01. Display welcome message
-print("Welcome! This is a computer gaming program of some sort! You'll enter your name, then make a menu selection, and be treated a lil message on the other end.")
-# 02. Get user name, store it 
-user_name = input("Your name: ")
-# 03. Display game menu
-print("1. Make Change","2. High Card","3. Quit",sep="\n")
-# 04. Get user choice, store it
-user_choice = int( input("Choose an option by entering the number you'd like to select. Then press enter. ") )
-# 05. Display output message
+# Display welcome message
+print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("| Welcome! This is the Lame Games Room! |")
+print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+# Get user name, store it 
+user_name = input("Say, what's your name? ")
+
+# Display game menu
+print()
+print('    Menu')
+print('~~~~~~~~~~~~')
+print("1. Make Change","2. High Card","3. Quit",sep="\n",end="\n\n")
+# Get user choice, store it
+user_choice = int( input("Choose an option: ") )
+# Display output message
 print(user_name, ", you chose menu option ", user_choice, sep="", end=".")
